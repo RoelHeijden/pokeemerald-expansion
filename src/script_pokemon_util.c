2ca51005510bcb532aa517cba3d7102e9f42a68e
RoelHeijden <roel.heijden@hotmail.com>
2024-11-14 13:00:00 +0100

    custom macro test

--- src/script_pokemon_util.c
+++ src/script_pokemon_util.c
@@ -561,3 +561,12 @@ void Script_SetStatus1(struct ScriptContext *ctx)
         SetMonData(&gPlayerParty[slot], MON_DATA_STATUS, &status1);
     }
 }
+
+// ADDED
+void Script_nMovesLearnt(struct ScriptContext *ctx)
+{
+    u32 slot = VarGet(ScriptReadHalfword(ctx));
+    u32 n = VarGet(ScriptReadHalfword(ctx));
+
+
+}
