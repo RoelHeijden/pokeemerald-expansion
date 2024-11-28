15dbf59c3d038ec4ee2ff123ebe21d9ef8a834d6
RoelHeijden <roel.heijden@hotmail.com>
2024-11-28 19:54:26 +0100

    wild encounter rework: Darkrai added

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -457,30 +457,69 @@ static void CreateWildMon(u16 species, u8 level)
     // ADDED
     // custom pokemon encounters for Escape room maps: main and ice puzzle
     u16 headerId = GetCurrentMapWildMonHeaderId();
-    if(gWildMonHeaders[headerId].mapNum == MAP_ESCAPE_ROOM)
-    {
-        u16 species = SPECIES_SMEARGLE;
-        u8 level = 20;
+    if(gWildMonHeaders[headerId].mapNum != MAP_ESCAPE_ROOM && gWildMonHeaders[headerId].mapNum != MAP_ESCAPE_ROOM_ICE_PUZZLE){
+        CreateMonWithNature(&gEnemyParty[0], species, level, USE_RANDOM_IVS, PickWildMonNature());
+    }
+    else{
+        u16 species;
+        u8 level;
         u8 abilityNum = 0;
-        u16 move1 = MOVE_SKETCH;
+        u16 heldItem = ITEM_NONE;
+        u16 move1 = MOVE_NONE;
         u16 move2 = MOVE_NONE;
         u16 move3 = MOVE_NONE;
         u16 move4 = MOVE_NONE;
-        u8 defEv = 208;
-        u8 hpIv = 11;
-        u8 atkIv = 0;
-        u8 defIv = 31;
-        u8 speedIv = 0;
+        u8 defEv = 0;
+        u8 hpIv = 31; 
+        u8 atkIv = 31; 
+        u8 defIv = 31; 
+        u8 speedIv = 31; 
         u8 spAtkIv = 31; 
-        u8 spDefIv = 31;
+        u8 spDefIv = 31; 
 
-        // Create a blank Pokémon
+        // Hardy nature = 0, so: (personality % 25) == 0
+        u32 personality = (Random32() / 25) * 25;
+
+        s16 x, y;
+        x = gObjectEvents[gPlayerAvatar.objectEventId].currentCoords.x;
+        y = gObjectEvents[gPlayerAvatar.objectEventId].currentCoords.y;
+
+        DebugPrintf("x, y: %d, %d", x, y);
+
+        if(gWildMonHeaders[headerId].mapNum == MAP_ESCAPE_ROOM)
+        {
+            if (y == 38 && x >= 15 && x <= 19) {  // 15-19, 38
+                species = SPECIES_DARKRAI;
+                level = 5;
+                abilityNum = 0;
+                move1 = MOVE_DARK_VOID;
+            }
+            else{
+                species = SPECIES_SMEARGLE;
+                level = 20;
+                abilityNum = 0;
+                move1 = MOVE_SKETCH;
+                defEv = 208;
+                hpIv = 11;
+                atkIv = 0;
+                speedIv = 0;
+
+                // Relaxed nature = 7, so: (personality % 25) == 7
+                personality = (Random32() / 25) * 25 + 7;
+            }
+        }
+        else if(gWildMonHeaders[headerId].mapNum == MAP_ESCAPE_ROOM_ICE_PUZZLE)
+        {
+            species = SPECIES_WEEZING_GALARIAN;
+            level = 15;
+            abilityNum = 1;
+            move1 = MOVE_PROTECT;
+            move2 = MOVE_EXPLOSION;
+            heldItem = ITEM_TM27;
+        }
+
+        // create new Pokémon
         ZeroMonData(&gEnemyParty[0]);
-
-        // Relaxed nature = 7, so: (personality % 25) == 7
-        u32 personality = (Random32() / 25) * 25 + 7;
-
-        // Initialize the Pokémon
         CreateBoxMon(&gEnemyParty[0].box, species, level, USE_RANDOM_IVS, TRUE, personality, OT_ID_PLAYER_ID, 0);
 
         // Set level and ability
@@ -504,61 +543,11 @@ static void CreateWildMon(u16 species, u8 level)
         SetMonData(&gEnemyParty[0], MON_DATA_MOVE3, &move3);
         SetMonData(&gEnemyParty[0], MON_DATA_MOVE4, &move4);
 
-        // Recalculate stats after modifications
-        CalculateMonStats(&gEnemyParty[0]);
-    }
-    else if(gWildMonHeaders[headerId].mapNum == MAP_ESCAPE_ROOM_ICE_PUZZLE)
-    {
-        u16 species = SPECIES_SHEDINJA;
-        u8 level = 10;
-        u8 abilityNum = 0;
-        u16 move1 = MOVE_PROTECT;
-        u16 move2 = MOVE_NONE;
-        u16 move3 = MOVE_NONE;
-        u16 move4 = MOVE_NONE;
-        u8 hpIv = 31;
-        u8 atkIv = 31;
-        u8 defIv = 31;
-        u8 speedIv = 31;
-        u8 spAtkIv = 31; 
-        u8 spDefIv = 31;
-        u16 heldItem = ITEM_FLAME_ORB;
-
-        // Create a blank Pokémon
-        ZeroMonData(&gEnemyParty[0]);
-
-        // Hardy nature = 0, so: (personality % 25) == 0
-        u32 personality = (Random32() / 25) * 25;
-
-        // Initialize the Pokémon
-        CreateBoxMon(&gEnemyParty[0].box, species, level, USE_RANDOM_IVS, TRUE, personality, OT_ID_PLAYER_ID, 0);
-
-        // Set level and ability
-        SetMonData(&gEnemyParty[0], MON_DATA_LEVEL, &level);
-        SetMonData(&gEnemyParty[0], MON_DATA_ABILITY_NUM, &abilityNum);
-
-        // Set IVs
-        SetMonData(&gEnemyParty[0], MON_DATA_HP_IV, &hpIv);
-        SetMonData(&gEnemyParty[0], MON_DATA_ATK_IV, &atkIv);
-        SetMonData(&gEnemyParty[0], MON_DATA_DEF_IV, &defIv);
-        SetMonData(&gEnemyParty[0], MON_DATA_SPEED_IV, &speedIv);
-        SetMonData(&gEnemyParty[0], MON_DATA_SPATK_IV, &spAtkIv);
-        SetMonData(&gEnemyParty[0], MON_DATA_SPDEF_IV, &spDefIv);
-
-        // Set moves
-        SetMonData(&gEnemyParty[0], MON_DATA_MOVE1, &move1);
-        SetMonData(&gEnemyParty[0], MON_DATA_MOVE2, &move2);
-        SetMonData(&gEnemyParty[0], MON_DATA_MOVE3, &move3);
-        SetMonData(&gEnemyParty[0], MON_DATA_MOVE4, &move4);
-
         // Set held item
         SetMonData(&gEnemyParty[0], MON_DATA_HELD_ITEM, &heldItem);
 
         // Recalculate stats after modifications
         CalculateMonStats(&gEnemyParty[0]);
-    }
-    else{
-        CreateMonWithNature(&gEnemyParty[0], species, level, USE_RANDOM_IVS, PickWildMonNature());
     }
 }
 #ifdef BUGFIX
