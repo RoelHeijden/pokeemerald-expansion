828012ab14338504dba05af3c59073a77ee5a9ef
RoelHeijden <roel.heijden@hotmail.com>
2024-12-12 02:24:02 +0100

    New ice puzzle and hole quiz

--- src/new_game.c
+++ src/new_game.c
@@ -133,7 +133,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(ESCAPE_ROOM), MAP_NUM(ESCAPE_ROOM), WARP_ID_NONE, 23, 20);
+    SetWarpDestination(MAP_GROUP(ESCAPE_ROOM), MAP_NUM(ESCAPE_ROOM), WARP_ID_NONE, 24, 20);
     WarpIntoMap();
 }
 
