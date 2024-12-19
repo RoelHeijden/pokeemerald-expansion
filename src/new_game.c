63eebe91d5940557950ff0bccfe9a283dc918d30
RoelHeijden <roel.heijden@hotmail.com>
2024-12-19 16:19:16 +0100

    Intro section added

--- src/new_game.c
+++ src/new_game.c
@@ -98,7 +98,7 @@ static void InitPlayerTrainerId(void)
 void SetDefaultOptions(void)
 {
     gSaveBlock2Ptr->optionsTextSpeed = OPTIONS_TEXT_SPEED_FAST;
-    gSaveBlock2Ptr->optionsWindowFrameType = 4;
+    gSaveBlock2Ptr->optionsWindowFrameType = 0;
     gSaveBlock2Ptr->optionsSound = OPTIONS_SOUND_MONO;
     gSaveBlock2Ptr->optionsBattleStyle = OPTIONS_BATTLE_STYLE_SET;
     gSaveBlock2Ptr->optionsBattleSceneOff = FALSE;
