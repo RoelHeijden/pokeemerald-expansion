4b7fec792ac39ea33d08df3487aba88955e39247
RoelHeijden <roel.heijden@hotmail.com>
2025-01-27 23:08:40 +0100

    map stuff

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -6,7 +6,8 @@
 //
 
 // gMapGroup_EscapeRoom2
-#define MAP_ESCAPE_ROOM_MAIN (0 | (0 << 8))
+#define MAP_ESCAPE_ROOM_MAIN             (0 | (0 << 8))
+#define MAP_ESCAPE_ROOM_ANNIHILAPES_LAIR (1 | (0 << 8))
 
 // gMapGroup_TownsAndRoutes
 #define MAP_PETALBURG_CITY      (0 | (1 << 8))
