ab5f9420dd9727959180f7ae24ad3f4fde7dacdd
RoelHeijden <roel.heijden@hotmail.com>
2025-02-10 01:53:57 +0100

    run from trainer battles fix

--- src/battle_util.c
+++ src/battle_util.c
@@ -456,11 +456,13 @@ bool32 TryRunFromBattle(u32 battler)
     u8 pyramidMultiplier;
     u8 speedVar;
 
-    // // ADDED
-    // // always success when trying to run
-    // gCurrentTurnActionNumber = gBattlersCount;
-    // gBattleOutcome = B_OUTCOME_RAN;
-    // return TRUE;
+    // ADDED
+    // always success when trying to run from trainers
+    if (gBattleTypeFlags & BATTLE_TYPE_TRAINER){
+        gCurrentTurnActionNumber = gBattlersCount;
+        gBattleOutcome = B_OUTCOME_RAN;
+        return TRUE;
+    }
 
 
     if (gBattleMons[battler].item == ITEM_ENIGMA_BERRY_E_READER)
