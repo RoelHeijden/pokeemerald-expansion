40666aad011456e4a97f4c91daebf062ef34953a
RoelHeijden <roel.heijden@hotmail.com>
2025-05-12 23:04:33 +0200

    tm taunt price 19k

--- src/data/items.h
+++ src/data/items.h
@@ -10616,7 +10616,7 @@ const struct Item gItemsInfo[] =
     [ITEM_TM_TAUNT] =
     {
         .name = _("TM12"),
-        .price = 40000,
+        .price = 19000,
         .description = COMPOUND_STRING(
             "Enrages the foe so\n"
             "it can only use\n"
