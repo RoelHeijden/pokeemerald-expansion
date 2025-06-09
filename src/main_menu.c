bf6bb039f5ff134173e1b853939b619e13139306
RoelHeijden <roel.heijden@hotmail.com>
2025-06-09 19:11:35 +0200

    birch intro enabled

--- src/main_menu.c
+++ src/main_menu.c
@@ -1066,8 +1066,10 @@ static void Task_HandleMainMenuAPressed(u8 taskId)
             default:
                 gPlttBufferUnfaded[0] = RGB_BLACK;
                 gPlttBufferFaded[0] = RGB_BLACK;
-                // gTasks[taskId].func = Task_NewGameBirchSpeech_Init;
-                gTasks[taskId].func = Task_NoIntro_Init;
+
+                // CHANGED
+                gTasks[taskId].func = Task_NewGameBirchSpeech_Init;
+                // gTasks[taskId].func = Task_NoIntro_Init;  BirchIntro disabled
                 break;
             case ACTION_CONTINUE:
                 gPlttBufferUnfaded[0] = RGB_BLACK;
