ea4fcdf92e8430e5ba568a297d1486f840e51f95
RoelHeijden <roel.heijden@hotmail.com>
2026-01-21 19:04:19 +0100

    Trainer11 moveset sign

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -785,14 +785,12 @@
 #define METATILE_Underwater_RockWall     0x21E
 
 // Other
+#define METATILE_EscapeRoomIceRock             0x39A
 #define METATILE_RSMossdeepGym_RedArrow_Down   0x205
 #define METATILE_RSMossdeepGym_RedArrow_Left   0x20C
 #define METATILE_RSMossdeepGym_RedArrow_Right  0x204
 #define METATILE_RSMossdeepGym_RedArrow_Up     0x20D
 #define METATILE_RSMossdeepGym_Switch_Down     0x239
 #define METATILE_RSMossdeepGym_Switch_Up       0x238
-
-// ADDED
-#define METATILE_EscapeRoomIceRock  0x39A
 
 #endif // GUARD_METATILE_LABELS_H
